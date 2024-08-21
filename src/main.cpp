38337a068d3d7a620ddc59e8e18ce9c1758663f9
Vladislav <jaday978@gmail.com>
2024-08-21 22:19:25 +0300

    Added Doxyden

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Vladislav (VLARINT3G@vk.com)
+ * @brief Этот файл производить бинарный поиск
+ * @version 0.1
+ * @date 2024-08-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
 #include <iostream>
 #include <vector>
 #include <algorithm>
@@ -5,8 +15,17 @@
 #include <ctime>
 #include "BinarySearch.h"
 
+/**
+ * @brief Основная функция, в которой происходит поиск масива string методом бинарного поиска 
+ */
+
 int main() {
-    std::srand(std::time(0)); // Инициализация генератора случайных чисел
+
+    /**
+     * @brief Вначале происходит Инициализация генератора случайных чисел, после формировние массива случайных чисел от 0 до 99 с последющим выводом
+     * 
+     */
+    std::srand(std::time(0)); 
 
     int n = 10; // Длина вектора
     std::vector<int32_t> main_arr(n);
@@ -14,29 +33,41 @@ int main() {
         main_arr[i] = std::rand() % 100; // Случайные числа в диапазоне от 0 до 99
     }
 
-    // Вывод случайного вектора длиной 10
+    /**
+     * @brief 
+     * 
+     */
     std::cout << "Original vector: ";
     for (const auto& num : main_arr) {
         std::cout << num << " ";
     }
     std::cout << std::endl;
 
-    // Сортировка вектора
+    /**
+     * @brief Далее происходит сортировака массива и вывод отсортированного массива
+     * 
+     */
     std::sort(main_arr.begin(), main_arr.end());
 
-    // Печать отсортированного вектора
     std::cout << "Sorted vector: ";
     for (const auto& num : main_arr) {
         std::cout << num << " ";
     }
     std::cout << std::endl;
 
-    // Значение для поиска
+    /**
+     * @brief Ицициализация значения, вводимого значения с клавиатуры
+     * 
+     */
     int user_target;
     std::cout << "Type down a digit for search: ";
     std::cin >> user_target;
 
-    // Создаем объект класса BinarySearch и выполняем поиск
+    /**
+     * @brief Использование объект класса BinarySearch и выполнение поиска
+
+     * 
+     */
     BinarySearch<int32_t> binary_search(main_arr);
     int result = binary_search.search(user_target);
 
--- tests/TestBinarySearch.cpp
+++ tests/TestBinarySearch.cpp
@@ -1,7 +1,10 @@
 #include "gtest/gtest.h"
 #include "../src/BinarySearch.h"
 
-
+/**
+ * @brief Тест проверяет, как бинарный поиск работает с массивом, содержащим отрицательные числа.
+ * 
+ */
 TEST(BinarySearchTest, HandlesNegativeValues) {
     SearchArray<int32_t> arr = {-10, -5, 0, 5, 10};
     BinarySearch<int32_t> binarySearch(arr);
@@ -12,7 +15,10 @@ TEST(BinarySearchTest, HandlesNegativeValues) {
     EXPECT_EQ(binarySearch.search(5), 3);
     EXPECT_EQ(binarySearch.search(10), 4);
 }
-
+/**
+ * @brief Тест проверяет работу бинарного поиска с массивом, содержащим дубликаты.
+ * 
+ */
 TEST(BinarySearchTest, HandlesDuplicateValues) {
     SearchArray<int32_t> arr = {1, 2, 2, 2, 3, 4, 5};
     BinarySearch<int32_t> binarySearch(arr);
@@ -23,7 +29,10 @@ TEST(BinarySearchTest, HandlesDuplicateValues) {
     EXPECT_EQ(binarySearch.search(0), 0); // 0 мог бы быть вставлен на индекс 0
     EXPECT_EQ(binarySearch.search(6), 7); // 6 мог бы быть вставлен на индекс 7
 }
-
+/**
+ * @brief Тест проверяет работу бинарного поиска на большом массиве, содержащем миллион элементов
+ * 
+ */
 TEST(BinarySearchTest, HandlesLargeArray) {
     const IndexType n = 1000000;
     SearchArray<int32_t> arr(n);
@@ -37,7 +46,10 @@ TEST(BinarySearchTest, HandlesLargeArray) {
     EXPECT_EQ(binarySearch.search(500000), 250000); // Элемент в середине
     EXPECT_EQ(binarySearch.search(1), 1);           // Нечетное число не в массиве, должно вернуть индекс 1
 }
-
+/**
+ * @brief Тест проверяет работу бинарного поиска на массиве, где все элементы одинаковы.
+ * 
+ */
 TEST(BinarySearchTest, HandlesSingleValueRepeated) {
     SearchArray<int32_t> arr = {7, 7, 7, 7, 7};
     BinarySearch<int32_t> binarySearch(arr);
@@ -46,7 +58,10 @@ TEST(BinarySearchTest, HandlesSingleValueRepeated) {
     EXPECT_EQ(binarySearch.search(6), 0);  // 6 мог бы быть вставлен на индекс 0
     EXPECT_EQ(binarySearch.search(8), 5);  // 8 мог бы быть вставлен на индекс 5
 }
-
+/**
+ * @brief Тест проверяет работу бинарного поиска с массивом строк.
+ * 
+ */
 TEST(BinarySearchTest, HandlesStrings) {
     SearchArray<std::string> arr = {"apple", "banana", "cherry", "date", "fig", "grape"};
     BinarySearch<std::string> binarySearch(arr);
