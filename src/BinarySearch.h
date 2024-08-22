68c56d2c4d6919bd21fff1d5f593a9c07c9f43bf
Vladislav <63453224+VLARINT3G@users.noreply.github.com>
2024-08-22 22:09:54 +0300

    Update BinarySearch.h

--- src/BinarySearch.h
+++ src/BinarySearch.h
@@ -1,59 +1,34 @@
+/**
+ * @file BinarySearch.h
+ * @author Vladislav
+ * @brief Класс BinarySearch для бинарного поиска в массиве целых чисел
+ * @version 0.1
+ * @date 2024-08-21
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
 #ifndef BINARY_SEARCH_H
 #define BINARY_SEARCH_H
 
 #include <vector>
 #include <cstdint>
 
-template <typename T>
-using SearchArray = std::vector<T>;
-
+using SearchArray = std::vector<int32_t>;
 using IndexType = std::size_t;
 
-template <typename T>
+/**
+ * @brief Класс BinarySearch реализует алгоритм бинарного поиска в массиве целых чисел.
+ */
 class BinarySearch {
 public:
-    explicit BinarySearch(const SearchArray<T>& arr);
-    IndexType search(const T& target);
-    void setArray(const SearchArray<T>& arr);
+    explicit BinarySearch(const SearchArray& arr);
+    IndexType search(int32_t target);
+    void setArray(const SearchArray& arr);
 
 private:
-    SearchArray<T> _searchArray;
+    SearchArray _searchArray;
 };
-
-template <typename T>
-BinarySearch<T>::BinarySearch(const SearchArray<T>& arr)
-    : _searchArray(arr) {
-}
-
-template <typename T>
-IndexType BinarySearch<T>::search(const T& target) {
-    IndexType left = 0;
-    IndexType right = _searchArray.size() - 1;
-
-    while (left < right) {
-        IndexType mid = left + (right - left) / 2;
-
-        if (target <= _searchArray[mid]) {
-            right = mid;
-        } else {
-            left = mid + 1;
-        }
-    }
-
-    if (_searchArray[left] == target) {
-        return left;
-    }
-
-    return left;
-}
-
-template <typename T>
-void BinarySearch<T>::setArray(const SearchArray<T>& arr) {
-    _searchArray = arr;
-}
-
-// Экземпляры шаблонов
-template class BinarySearch<int32_t>;
-template class BinarySearch<std::string>;
 
 #endif  // BINARY_SEARCH_H
