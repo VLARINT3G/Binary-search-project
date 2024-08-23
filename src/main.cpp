3e3f88667713acabf3890b026e98d9e6accc78df
Vladislav <63453224+VLARINT3G@users.noreply.github.com>
2024-08-23 10:19:17 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,13 @@
 #include "BinarySearch.h"
 
 BinarySearch::BinarySearch(const SearchArray& arr)
-    : _searchArray(arr) {
-}
+    : _searchArray(arr) {}
 
 IndexType BinarySearch::search(int32_t target) {
-    IndexType left = 0;
-    IndexType right = _searchArray.size() - 1;
+    IndexType left = 0, right = _searchArray.size() - 1;
 
     while (left < right) {
         IndexType mid = left + (right - left) / 2;
-
         if (target <= _searchArray[mid]) {
             right = mid;
         } else {
@@ -18,11 +15,7 @@ IndexType BinarySearch::search(int32_t target) {
         }
     }
 
-    if (_searchArray[left] == target) {
-        return left;
-    }
-
-    return left;
+    return (_searchArray[left] == target) ? left : left;
 }
 
 void BinarySearch::setArray(const SearchArray& arr) {
