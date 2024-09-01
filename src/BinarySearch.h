2b38633eb55d15e4956ceeaf91991c931826db82
Vladislav <63453224+VLARINT3G@users.noreply.github.com>
2024-09-01 23:07:41 +0300

    Update BinarySearch.h

--- src/BinarySearch.h
+++ src/BinarySearch.h
@@ -1,34 +1,48 @@
 /**
  * @file BinarySearch.h
- * @author Vladislav
- * @brief Класс BinarySearch для бинарного поиска в массиве целых чисел
- * @version 0.1
- * @date 2024-08-21
- * 
- * @copyright Copyright (c) 2024
- * 
+ * @brief Заголовочный файл для класса BinarySearch.
+ *
+ * Этот файл содержит описание класса BinarySearch, который реализует
+ * бинарный поиск в отсортированном массиве целых чисел.
  */
 
-#ifndef BINARY_SEARCH_H
-#define BINARY_SEARCH_H
+#pragma once
 
 #include <vector>
-#include <cstdint>
+#include <cstddef> // Для size_t и ptrdiff_t
+#include <cstdint> // Для int32_t
 
-using SearchArray = std::vector<int32_t>;
-using IndexType = std::size_t;
+using IndexType = std::size_t; ///< Тип индекса для массива.
+using SearchArray = std::vector<int32_t>;  ///< Определение типа для использования вектора целых чисел.
 
 /**
- * @brief Класс BinarySearch реализует алгоритм бинарного поиска в массиве целых чисел.
+ * @class BinarySearch
+ * @brief Класс, реализующий алгоритм бинарного поиска.
+ *
+ * Этот класс предоставляет методы для выполнения бинарного поиска
+ * в отсортированном массиве целых чисел.
  */
 class BinarySearch {
 public:
+    /**
+     * @brief Конструктор класса BinarySearch.
+     * @param arr Входной массив для инициализации объекта.
+     */
     explicit BinarySearch(const SearchArray& arr);
-    IndexType search(int32_t target);
+
+    /**
+     * @brief Метод для выполнения бинарного поиска.
+     * @param target Число, которое необходимо найти в массиве.
+     * @return Индекс числа в массиве или размер массива, если число не найдено.
+     */
+    IndexType search(int32_t target) const;
+
+    /**
+     * @brief Метод для установки нового массива.
+     * @param arr Новый массив для поиска.
+     */
     void setArray(const SearchArray& arr);
 
 private:
-    SearchArray _searchArray;
+    SearchArray _searchArray;  ///< Внутренний массив, используемый для выполнения бинарного поиска.
 };
-
-#endif  // BINARY_SEARCH_H
