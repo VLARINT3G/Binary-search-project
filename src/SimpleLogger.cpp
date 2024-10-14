e6cc80c4e5d1b50ff68e43365910dd411c90ae0e
Vladislav <63453224+VLARINT3G@users.noreply.github.com>
2024-10-14 21:20:43 +0300

    Update SimpleLogger.cpp

--- src/SimpleLogger.cpp
+++ src/SimpleLogger.cpp
@@ -1,17 +1,34 @@
 #include "SimpleLogger.h"
+#include <fstream>
+#include <ctime>
+#include <iostream>
 
-/**
- * @brief Запись сообщения в файл "binary_search.log".
- *
- * Открывает файл для записи (в режиме добавления), записывает сообщение, и закрывает файл.
- *
- * @param message Строка сообщения для записи в лог.
- */
-void SimpleLogger::log(const std::string& message) {
-    std::ofstream logFile;
-    logFile.open("binary_search.log", std::ios::out | std::ios::app);
+void SimpleLogger::log(const std::string& message, LogLevel level) {
+    std::ofstream logFile("binary_search.log", std::ios::out | std::ios::app);
     if (logFile.is_open()) {
-        logFile << message << std::endl;
-        logFile.close();
+        logFile << "[" << getCurrentTimestamp() << "] "
+                << "[" << getLogLevelString(level) << "] "
+                << message << std::endl;
     }
+}
+
+std::string SimpleLogger::getLogLevelString(LogLevel level) {
+    switch (level) {
+        case LogLevel::INFO:
+            return "INFO";
+        case LogLevel::WARNING:
+            return "WARNING";
+        case LogLevel::ERROR:
+            return "ERROR";
+        default:
+            return "UNKNOWN";
+    }
+}
+
+std::string SimpleLogger::getCurrentTimestamp() {
+    auto now = std::time(nullptr);
+    auto tm = *std::localtime(&now);
+    char buffer[20];
+    std::strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", &tm);
+    return buffer;
 }
